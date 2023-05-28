867444696ff80cac3791cc4c0635efe379ebd73a
Floride125-art <flo.tuyisenge@gmail.com>
2023-05-28 15:27:00 +0530

    customized convas, resize, and objects

--- src/Canvas.js
+++ src/Canvas.js
@@ -10,7 +10,7 @@ const Canvas = () => {
   const [dragOffsetX, setDragOffsetX] = useState(0);
   const [dragOffsetY, setDragOffsetY] = useState(0);
   const [resizing, setResizing] = useState(false);
-  const [resizeHandle, setResizeHandle] = useState(null);
+  const [resizeHandle, setResizeHandle] = useState('');
 
   const handleDragStart = (e, id) => {
     e.dataTransfer.setData('text/plain', id);
@@ -44,34 +44,41 @@ const Canvas = () => {
     e.preventDefault();
     setActiveObjectId(id);
 
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
     const object = objects.find((obj) => obj.id === id);
-    if (object) {
-      const canvas = canvasRef.current;
-      const rect = canvas.getBoundingClientRect();
-      const mouseX = e.clientX - rect.left;
-      const mouseY = e.clientY - rect.top;
-      const objX = object.x;
-      const objY = object.y;
-      const objWidth = object.width;
-      const objHeight = object.height;
-      const handleRadius = 10; 
-      if (
-        mouseX >= objX + objWidth - handleRadius &&
-        mouseX <= objX + objWidth + handleRadius &&
-        mouseY >= objY + objHeight - handleRadius &&
-        mouseY <= objY + objHeight + handleRadius
-      ) {
-   
-        setResizing(true);
-        setResizeHandle('bottom-right');
-        setDragOffsetX(objWidth - (mouseX - objX));
-        setDragOffsetY(objHeight - (mouseY - objY));
-      } else {
-        setResizing(false);
-        setResizeHandle(null);
-        setDragOffsetX(mouseX - objX);
-        setDragOffsetY(mouseY - objY);
-      }
+    const objectX = object.x;
+    const objectY = object.y;
+    const objectWidth = object.width;
+    const objectHeight = object.height;
+
+    const resizeHandles = {
+      topLeft: { x: objectX, y: objectY },
+      topRight: { x: objectX + objectWidth, y: objectY },
+      bottomLeft: { x: objectX, y: objectY + objectHeight },
+      bottomRight: { x: objectX + objectWidth, y: objectY + objectHeight },
+    };
+
+    if (isWithinResizeHandle(mouseX, mouseY, resizeHandles.topLeft)) {
+      setResizing(true);
+      setResizeHandle('topLeft');
+    } else if (isWithinResizeHandle(mouseX, mouseY, resizeHandles.topRight)) {
+      setResizing(true);
+      setResizeHandle('topRight');
+    } else if (isWithinResizeHandle(mouseX, mouseY, resizeHandles.bottomLeft)) {
+      setResizing(true);
+      setResizeHandle('bottomLeft');
+    } else if (isWithinResizeHandle(mouseX, mouseY, resizeHandles.bottomRight)) {
+      setResizing(true);
+      setResizeHandle('bottomRight');
+    } else {
+      setDragOffsetX(mouseX - objectX);
+      setDragOffsetY(mouseY - objectY);
+      setResizing(false);
+      setResizeHandle('');
     }
   };
 
@@ -81,17 +88,14 @@ const Canvas = () => {
       const rect = canvas.getBoundingClientRect();
       const mouseX = e.clientX - rect.left;
       const mouseY = e.clientY - rect.top;
+
       setObjects((prevObjects) => {
         return prevObjects.map((obj) => {
           if (obj.id === activeObjectId) {
             if (resizing) {
-              const newWidth = mouseX - obj.x + dragOffsetX;
-              const newHeight = mouseY - obj.y + dragOffsetY;
-              return { ...obj, width: newWidth, height: newHeight };
+              return resizeObject(obj, mouseX, mouseY);
             } else {
-              const newX = mouseX - dragOffsetX;
-              const newY = mouseY - dragOffsetY;
-              return { ...obj, x: newX, y: newY };
+              return moveObject(obj, mouseX, mouseY);
             }
           }
           return obj;
@@ -103,7 +107,47 @@ const Canvas = () => {
   const handleMouseUp = () => {
     setActiveObjectId(null);
     setResizing(false);
-    setResizeHandle(null);
+    setResizeHandle('');
+  };
+
+  const isWithinResizeHandle = (x, y, handle) => {
+    const handleSize = 8; 
+    return x >= handle.x - handleSize && x <= handle.x + handleSize && y >= handle.y - handleSize && y <= handle.y + handleSize;
+  };
+
+  const resizeObject = (obj, mouseX, mouseY) => {
+    const objectX = obj.x;
+    const objectY = obj.y;
+    const objectWidth = obj.width;
+    const objectHeight = obj.height;
+
+    let newWidth = objectWidth;
+    let newHeight = objectHeight;
+
+    switch (resizeHandle) {
+      case 'topLeft':
+        newWidth = objectX + objectWidth - mouseX;
+        newHeight = objectY + objectHeight - mouseY;
+        return { ...obj, x: mouseX, y: mouseY, width: newWidth, height: newHeight };
+      case 'topRight':
+        newWidth = mouseX - objectX;
+        newHeight = objectY + objectHeight - mouseY;
+        return { ...obj, y: mouseY, width: newWidth, height: newHeight };
+      case 'bottomLeft':
+        newWidth = objectX + objectWidth - mouseX;
+        newHeight = mouseY - objectY;
+        return { ...obj, x: mouseX, width: newWidth, height: newHeight };
+      case 'bottomRight':
+        newWidth = mouseX - objectX;
+        newHeight = mouseY - objectY;
+        return { ...obj, width: newWidth, height: newHeight };
+      default:
+        return obj;
+    }
+  };
+
+  const moveObject = (obj, mouseX, mouseY) => {
+    return { ...obj, x: mouseX - dragOffsetX, y: mouseY - dragOffsetY };
   };
 
   const renderObjects = () => {
@@ -120,20 +164,96 @@ const Canvas = () => {
           width: obj.width,
           height: obj.height,
           background: obj.color,
-          cursor: 'move',
-          border: '1px solid black',
-          boxSizing: 'border-box',
-          resize: 'both',
-          overflow: 'auto',
+          cursor: resizing ? getResizeCursor() : 'move',
+          border: resizing ? '1px dashed black' : 'none',
         }}
-      />
+      >
+        {resizing && renderResizeHandles(obj)}
+      </div>
     ));
+  };
+
+  const getResizeCursor = () => {
+    switch (resizeHandle) {
+      case 'topLeft':
+      case 'bottomRight':
+        return 'nwse-resize';
+      case 'topRight':
+      case 'bottomLeft':
+        return 'nesw-resize';
+      default:
+        return 'auto';
+    }
+  };
+
+  const renderResizeHandles = (obj) => {
+    const handleSize = 8; 
+
+    return (
+      <>
+        <div
+          style={{
+            position: 'absolute',
+            left: obj.x - handleSize / 2,
+            top: obj.y - handleSize / 2,
+            width: handleSize,
+            height: handleSize,
+            background: 'white',
+            border: '1px solid black',
+            cursor: 'nwse-resize',
+          }}
+        />
+        <div
+          style={{
+            position: 'absolute',
+            left: obj.x + obj.width - handleSize / 2,
+            top: obj.y - handleSize / 2,
+            width: handleSize,
+            height: handleSize,
+            background: 'white',
+            border: '1px solid black',
+            cursor: 'nesw-resize',
+          }}
+        />
+        <div
+          style={{
+            position: 'absolute',
+            left: obj.x - handleSize / 2,
+            top: obj.y + obj.height - handleSize / 2,
+            width: handleSize,
+            height: handleSize,
+            background: 'white',
+            border: '1px solid black',
+            cursor: 'nesw-resize',
+          }}
+        />
+        <div
+          style={{
+            position: 'absolute',
+            left: obj.x + obj.width - handleSize / 2,
+            top: obj.y + obj.height - handleSize / 2,
+            width: handleSize,
+            height: handleSize,
+            background: 'white',
+            border: '1px solid black',
+            cursor: 'nwse-resize',
+          }}
+        />
+      </>
+    );
   };
 
   return (
     <div
       ref={canvasRef}
-      style={{ position: 'relative', width: '800px', height: '600px', border: '1px solid black' }}
+      style={{
+        position: 'relative',
+        width: '800px',
+        height: '600px',
+        border: '1px solid black',
+        backgroundColor: '#f0f0f0',
+        overflow: 'hidden',
+      }}
       onDrop={handleDrop}
       onDragOver={handleDragOver}
       onMouseMove={handleMouseMove}
